593bf8b5f10afedd8e514d1baf575cdb80c17db2
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-06-11 21:28:45 -0300

    Criando Exercicio da Aula 12

---
+++ Atividade-Testes/TestProjectAtv12/Aluno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectAtv12
+{
+    public class Aluno
+    {    
+        public string Nome { get; set; }
+        public string Snome /*sobrenome*/ { get; set; }
+        public int Codigo { get; set; }
+        public DateTime DataNasc { get; set; }
+
+        public DateTime Data;
+
+        public int Idade()
+        {
+            var c = DateTime.Now;
+            Data = c;
+            var i = Convert.ToInt32(Data.Year - DataNasc.Year);
+            return i;
+        }
+
+        public string nomeCompleto()
+        {
+            return "" + Nome + " " + Snome + "";
+        }
+
+        public int validarNome()
+        {
+            int i = 0;
+            string n = Nome;
+            string s = Snome;
+
+            if (n.Contains(" Ç") || n.Contains(" ç") || s.Contains(" Ç") || s.Contains(" ç"))
+            {
+                Console.WriteLine("Este nome não pode ser registrado. Contém a letra no início 'Ç' que é inválido ");
+                i = +1;
+            }
+            if (n.Contains("@") || s.Contains("@"))
+            {
+                Console.WriteLine("Este caracter não podeser utilizado. O caracter '@' é inválido");
+                i = +2;
+            }
+            return i;
+        }
+
+        public string ValidacaoErro(int i)
+        {
+            string m;
+            if (validarNome() == 1)
+            {
+                m = "ERROR POR 'Ç'";
+                return m;
+            }
+            else if (validarNome() == 2)
+            {
+                m = "ERROR POR '@'";
+                return m;
+            }
+
+            return " ";
+        }
+    }
+}
